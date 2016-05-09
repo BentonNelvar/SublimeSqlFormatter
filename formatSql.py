a3b49561a83d8bcfdad8d8d3a877442565457d3d
Piotr Gajowniczek <piotrgajow@gmail.com>
2016-05-09 23:23:13 +0200

    Change behaviour to capitalizing each word in the file

--- formatSql.py
+++ formatSql.py
@@ -2,5 +2,14 @@ import sublime, sublime_plugin
   
 class FormatSqlCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-    	for selector in self.view.sel():
-        	self.view.insert(edit, selector.begin(), "Hello, World!\n")
+        view = self.view
+        index = 0
+        while( index < view.size() ):
+            region , word = getWord( view , index )
+            view.replace( edit , region , word.upper() )
+            index = region.b + 1
+
+def getWord( view , index ):
+    region = view.word( index )
+    word = view.substr( region )
+    return ( region , word )
