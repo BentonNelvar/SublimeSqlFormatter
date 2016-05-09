126ea210c54add7286667f6a9451d19638ec858a
Piotr Gajowniczek <piotrgajow@gmail.com>
2016-05-09 21:26:25 +0200

    Refactor command name to FormatSql

--- formatSql.py
+++ formatSql.py
@@ -1,5 +1,5 @@
 import sublime, sublime_plugin  
   
-class HelloWorldCommand(sublime_plugin.TextCommand):
+class FormatSqlCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         self.view.insert(edit, 0, "Hello, World!\n")
